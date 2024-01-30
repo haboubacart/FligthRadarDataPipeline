ed7476a718431d061ec2cf764ff0b1ee72629dd3
haboubacart <haboubacart@gmail.com>
2024-01-30 23:39:45 +0100

    Ajout extractor et loader

--- ETL spark/src/extractor.py
+++ ETL spark/src/extractor.py
@@ -1,6 +1,10 @@
 from logger import setup_logger
 import json
+import time
 from FlightRadar24 import FlightRadar24API
+from loader import save_data_txt
+from loader import save_data_json
+
 logger = setup_logger()
 
 
@@ -45,19 +49,28 @@ def extract_flights_data(fr_api, airlines_data):
     Returns:
         json : données des vols
     """
+    
     try:
-        logger.info("Début d'extraction des données des aéroports")
-        for flight in airlines_data["ICAO"]:
-            flight_details = fr_api.get_flight_details(flight)
-            flight.set_flight_details(flight_details)
-            flight_origin = flight.origin_airport_icao
-            flight_destination =  flight.destination_airport_icao
-            ########
-            flights_data = fr_api.get_airports()
-            logger.info("Fin d'extraction des données des aéroports")
-            return flights_data
+        logger.info("Début d'extraction des données de vols")
+        flight_details_list = []
+        flight_list = []
+        nb_max = 5
+        for airline in airlines_data[30:40]:
+            airline_flights_list = fr_api.get_flights(airline["ICAO"])
+            if len(airline_flights_list) > nb_max:
+                airline_flights_list = airline_flights_list[0:nb_max]           
+            for flight in airline_flights_list:
+                flight_details = fr_api.get_flight_details(flight)
+                flight.set_flight_details(flight_details)
+                flight_origin = flight.origin_airport_icao
+                flight_destination = flight.destination_airport_icao                
+
+                flight_details_list.append(flight_origin + "#" + flight_destination)
+                flight_list.append(flight)
+        logger.info("Fin d'extraction des données de vols")
+        return (flight_list, flight_details_list)
     except : 
-        logger.info("Une erreur est survenue pendant l'extraction des données des aéroports")
+        logger.info("Une erreur est survenue pendant l'extraction des données de vols")
         return None
     
     
@@ -70,16 +83,37 @@ def exract_zones_data(fr_api):
     """
     try:
         logger.info("Début d'extraction des données des zones")
-        zones_data = fr_api.get_airports()
+        zones_data = fr_api.get_zones()
         logger.info("Fin d'extraction des données des zones")
         return zones_data
     except : 
         logger.info("Une erreur est survenue pendant l'extraction des données des zones")
         return None
 
+def extract_and_save_data():
+    fr_api = FlightRadar24API()
+    airlines_data = extract_airlines_data(fr_api)
+    (flights_data, flight_details) = extract_flights_data(fr_api, airlines_data)
+    airports_data = extract_airports_data(fr_api)
+    zones_data = exract_zones_data(fr_api)
+    
+    try :
+        save_data_txt(flights_data, "Flights", "rawzone")
+        save_data_txt(flight_details, "Flights_details", "rawzone")
+        logger.info("Données de vols chargées avec succès dans la rawzone")
 
-"""
-fr_api = FlightRadar24API()
-extract_airlines_data(fr_api)
-extract_airports_data(fr_api)
-"""    
+        save_data_json(airlines_data, "Airlines", "rawzone")
+        logger.info("Données des compagnies aériennes chargées avec succès dans la rawzone")
+
+        save_data_json(zones_data, "Zones", "rawzone")
+        logger.info("Données des zones chargées avec succès dans la rawzone")
+
+        save_data_txt(airports_data, "Airports", "rawzone")
+        logger.info("Données des aéroports chargées avec succès dans la rawzone")
+
+        
+    except :
+        logger.error("Une erreur est survenue lors du chargement des données dans la rawzone")
+    
+    
+extract_and_save_data()
--- ETL spark/src/loader.py
+++ ETL spark/src/loader.py
@@ -1,21 +1,28 @@
 import os
+import json
 import datetime
 from logger import setup_logger
 
 logger = setup_logger()
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[:-8]
 
-
-def create_folder_if_not_exists(data_folder_path):   
-    created_folder = 'Data_extracted/'+ data_folder_path + '/tech_year='+str(datetime.date.today()).split('-')[0]+'/tech_month='+str(datetime.date.today()).split('-')[1]+ '/tech_day='+str(datetime.date.today())+'/'
+def create_folder_if_not_exists(data_folder_path, data_layer):   
+    created_folder = 'Data_extracted/'+ data_folder_path + '/' + data_layer +'/tech_year='+str(datetime.date.today()).split('-')[0]+'/tech_month='+str(datetime.date.today()).split('-')[1]+ '/tech_day='+str(datetime.date.today())+'/'
     try:
         os.makedirs(created_folder, exist_ok=True)
         print('created folder ', created_folder)
     except :
         pass 
     return created_folder
-        
 
-def save_data_to_csv(data_folder_path, data_df):
-    created_folder = create_folder_if_not_exists(data_folder_path)
-    data_df.to_csv(os.path.join(created_folder, data_folder_path+'_5.csv'), index=False)
+def save_data_txt(data, filename, data_layer):
+    data_folder = create_folder_if_not_exists(filename, data_layer)
+    with open(data_folder + filename + timestamp + ".txt", "w") as file:
+        for item in data:
+            file.write(str(item)+"\n")
+
+def save_data_json(data, filename, data_layer):
+    data_folder = create_folder_if_not_exists(filename, data_layer)
+    with open(data_folder + filename + timestamp + ".json", "w") as file:
+        json.dump(data, file, indent=2)
     
